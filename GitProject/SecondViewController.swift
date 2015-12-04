e9662efaa2349793a6b185cd416f97fef5d409cc
bheftye <bheftye92@gmail.com>
2015-12-03 21:58:38 -0600

    Initial Commit

---
+++ GitProject/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  GitProject
+//
+//  Created by Brent Heftye on 03/12/15.
+//  Copyright © 2015 Heftye Developers. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
